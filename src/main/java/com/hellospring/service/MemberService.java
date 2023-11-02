e5cdaf4b7f1d2ecbe61692f7e3db1f76db0917fe
kokojihye <jihyevivianako@gmail.com>
2023-11-02 16:54:14 +0900

    feat : 회원 관리를 위한 회원 등록 비즈니스 로직 작성

---
+++ src/main/java/com/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package com.hellospring.service;
+
+import com.hellospring.domain.Member;
+import com.hellospring.repository.MemberRepository;
+import com.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    * 회원 가입
+    * */
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //findByName이 <Optional>로 리턴하기 때문에 추가적인 처리 없이 .ifPresent()를 사용할 수 있다.
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
