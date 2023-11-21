e108d784eeab74f27cf95f73f439b2cb9c32ae4b
kokojihye <jihyevivianako@gmail.com>
2023-11-21 13:54:18 +0900

    feat: 순수 jdbc, jdbcTemplate방식 구현

---
+++ src/main/java/com/hellospring/SpringConfig.java
@@ -0,0 +1,33 @@
+package com.hellospring;
+
+import com.hellospring.repository.JdbcTemplateMemberRepository;
+import com.hellospring.repository.MemberRepository;
+import com.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+}
--- src/test/java/com/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,30 +1,44 @@
 package com.hellospring.service;
 
 import com.hellospring.domain.Member;
+import com.hellospring.repository.MemberRepository;
 import com.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+@SpringBootTest
+@Transactional //테스트 진행 시 롤백처리
+class MemberServiceIntegrationTest {
+    //integration test(통합 테스트)
 
-class MemberServiceTest {
-    //unit test(단위 테스트)
-
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    /*
+    * @BeforeEach
+    * public void beforeEach() {
+    *   memberRepository = new MemoryMemberRepository();
+    *   memberService = new MemberService(memberRepository);
+    * }
+    *
+    * 직접 객체를 생성하는 것이 아닌, 컨테이너에게 MemberService, MemberRepository를 받아야 한다.
+    * */
 
+   /*
+   * @AfterEach
+   * public void afterEach() {
+   *    memberRepository.clearStore();
+   * }
+   *
+   * MemoryDB에 있던 데이터가 다음 테ㅅ트에 영향을 주는 것을 막기 위해 작성했던 코드
+   * 테스트는 모두 독립적이라는 것을 기억하자
+   * */
 
     @Test
     public void 회원가입() {
@@ -54,21 +68,6 @@ class MemberServiceTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//        try {
-//            memberService.join(member2);
-//            fail();
-//        } catch (IllegalStateException e) {
-//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//        }
-
-        //then
     }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
